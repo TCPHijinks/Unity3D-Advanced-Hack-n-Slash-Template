ee160a8509a99b41dcb41bdbfdf6f99248734f75
PhylacteryLost <chodge.iam@gmail.com>
2020-01-27 16:21:11 +1000

    Initial project commit and .gitignore update.
    Added new Unity project files to the repo with basic controller input and primitive player movement working.
    
    All will be subject to a lot of experimentation and change going forth.
    
    Also updated .gitignore to an alternative to the default to properly ignore .meta files.

---
+++ Ranma Game/Assets/Scripts/Character.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    protected CharacterController cControl;
+    [SerializeField] protected float moveSpeed = 0;
+
+
+
+   
+    protected void MoveAndRotate(Vector2 moveDirRequest)
+    {       
+        // No move/rotation if not moving.
+        if (moveDirRequest == Vector2.zero) return;
+               
+        // Don't move/rotate if too slow.
+        if (Mathf.Abs(moveDirRequest.sqrMagnitude) < deadZone) return;
+        
+        // No diagonal movement. Move horizontal OR vertical.
+        if (Mathf.Abs(moveDirRequest.x) > Mathf.Abs(moveDirRequest.y))
+        {
+            moveDirRequest.y = 0;
+            ApplyRotation(new Vector3(RoundToNonZero(moveDirRequest.x), 0, 0));
+        }
+        else
+        {
+            moveDirRequest.x = 0;
+            ApplyRotation(new Vector3(0, 0, RoundToNonZero(moveDirRequest.y)));
+        }
+
+        Vector3 moveDir = new Vector3(moveDirRequest.x * moveSpeed, 0, moveDirRequest.y * moveSpeed);
+        moveDir = Vector3.Lerp(transform.position, moveDir, 1);
+        cControl.Move(moveDir);       
+    }
+    private readonly float deadZone = .05f;
+
+
+
+
+
+    private void ApplyRotation(Vector3 lookDir)
+    {
+        transform.rotation = Quaternion.LookRotation(lookDir);             
+    }
+
+
+
+
+
+    /// <summary>
+    /// Returns next integer away from 0.
+    /// </summary>
+    /// <param name="toRound"></param>
+    /// <returns></returns>
+    private int RoundToNonZero(float toRound)
+    {
+        if (toRound > 0) return Mathf.CeilToInt(toRound);
+        else return Mathf.FloorToInt(toRound);
+    }
+}
---
+++ Ranma Game/Assets/Scripts/PlayerChar.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChar : Character
+{
+    private PlayerControls controls;
+    private Vector2 moveDir = Vector2.zero;
+    private void Awake()
+    {
+        cControl = GetComponent<CharacterController>();
+
+        controls = new PlayerControls();
+        controls.Gameplay.Maneuver.performed += ctx => Maneuver();
+        controls.Gameplay.AttackStd.performed += ctx => AttackStd();
+        controls.Gameplay.AttackHeavy.performed += ctx => AttackHeavy();
+        controls.Gameplay.Interact.performed += ctx => Interact();
+
+        controls.Gameplay.Move.performed += ctx => moveDir = ctx.ReadValue<Vector2>();//  moveDir = new Vector3(ctx.ReadValue<Vector2>().x, 0, ctx.ReadValue<Vector2>().y);
+        controls.Gameplay.Move.canceled += ctx => moveDir = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        // Don't move if attacking.
+        MoveAndRotate(moveDir);
+    }
+
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+
+    void Maneuver()
+    {
+        Debug.Log("JUMP! ROLL!");
+    }
+
+    void AttackStd()
+    {
+        Debug.Log("BASIC BITCH PUNCH!");
+    }
+
+    void AttackHeavy()
+    {
+        Debug.Log("KICK TO THE DICK");
+    }
+
+    void Interact()
+    {
+        Debug.Log("JUST USE IT!");
+    }
+}
