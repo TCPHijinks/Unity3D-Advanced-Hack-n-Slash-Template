ef2dace6558d07594d26417d8e68dc7a05c00ec4
PhylacteryLost <chodge.iam@gmail.com>
2020-02-06 06:38:10 +1000

    Alternative std attack & support for soft combos
    When tapping the standard attack button, its animation will alternate
    between a left and right handed punch animation. This system enables
    chains of animations to be used as combos. However, the old alternative
    hit system needs to be removed as this new one replaces that ones
    intended implementation. Furthermore, the ability to do a "charged"
    attack after transitioning to an alt handed attack Must be added.

--- Ranma Game/Assets/Scripts/Character/CharacterAnimManager.cs
+++ Ranma Game/Assets/Scripts/Character/CharacterAnimManager.cs
@@ -24,9 +24,14 @@ public class CharacterAnimManager : MonoBehaviour
 
     public void DoAttack(AttkType attackType)
     {
+        altAnimIfTwo++;
+
         anim.SetInteger("AttackType", (int)attackType);
-        if (attackType == AttkType.standard) _doChargedAttk = !_doChargedAttk;
+        if (!DoingAttack && !inBaseAnimTransition && attackType == AttkType.standard) _doChargedAttk = !_doChargedAttk;
     }
+
+    private bool canDoCharged = false;
+    private int altAnimIfTwo = 0;
 
     public void CancelChargedAttack()
     {
@@ -37,14 +42,33 @@ public class CharacterAnimManager : MonoBehaviour
 
     private void Update()
     {
+        inBaseAnimTransition = anim.IsInTransition(0);
+        if (inBaseAnimTransition) startedBaseTransition = true;
+
+        if (altAnimIfTwo >= 2 && DoingAttack && startedBaseTransition && !inBaseAnimTransition)
+        {
+            altAnimIfTwo = 0;
+            anim.SetBool("AltAttackAnim", anim.GetBool("AltAttackAnim") == false);
+        }
     }
+
+    private bool startedBaseTransition = false;
 
     private void LateUpdate()
     {
-        if (!CanDoDamage && !_doChargedAttk) anim.SetInteger("AttackType", (int)AttkType.none);
+        //  if (!CanDoDamage && !_doChargedAttk) anim.SetInteger("AttackType", (int)AttkType.none);
         InAttkComboAndCanMove = anim.GetCurrentAnimatorStateInfo(0).IsTag("CanMoveAttack");
         DoingAttack = anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack") || InAttkComboAndCanMove;
         AnimMoveSpeedPenalty = anim.GetFloat("MoveSpeedPenaltyPercentage");
         anim.SetBool("ChargedAttack", _doChargedAttk);
+
+        // If transition just finished to a new attack, reset attack in prep for next one.
+        if (startedBaseTransition && !inBaseAnimTransition && anim.GetInteger("AttackType") != (int)AttkType.none)
+        {
+            startedBaseTransition = false;
+            anim.SetInteger("AttackType", (int)AttkType.none);
+        }
     }
+
+    private bool inBaseAnimTransition = false;
 }
